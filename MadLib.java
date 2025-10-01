4078a3836acf2c1332d5ef6310b44041a3cda621
frostghost555 <83512260+frostghost555@users.noreply.github.com>
2025-10-01 11:03:42 -0700

    Update MadLib.java
    1) completed madlibs story (added more content)
    2) added more substitutions
    3) added comments

--- MadLib.java
+++ MadLib.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
+import java.util.Scanner; // import scanner class (to create a scanner object).
 
-public class Madlib {
+public class Senapati_Madlibs {
     public static void main(String[] args) {
+        // Create a scanner object (to collect input).
         Scanner scanner = new Scanner(System.in);
+
+        // Collect user input.
         System.out.print("Enter a noun: ");
         String noun = scanner.nextLine();
         System.out.print("Enter a verb: ");
@@ -15,6 +18,8 @@ public class Madlib {
         String verb2 = scanner.nextLine();
         System.out.print("Enter another adjective: ");
         String adjective2 = scanner.nextLine();
-        System.out.print("You are a " + adjective + " " + noun + " who always " + verb + " everday. How do you do it? How does one man muster up the courage to commit such atrocities? You are not a god, yet alone a " + noun2 + "." + "You shall be cast into the depth of Helheim, denying yourself all chances of entry into Valhalla.");
+
+        // Generate MadLib Story.
+        System.out.print("You are a " + adjective + " " + noun + " who always " + verb + " everyday. How do you do it? How does one man muster up the courage to commit such atrocities? You are not a god, yet alone a " + noun2 + ". " + "You shall be cast into the depth of Helheim, denying yourself all chances of entry into Valhalla." + " There, you will " + verb2 + " among the shadows, forever tormented by your " + adjective2 + " deeds.");
     }
 }
